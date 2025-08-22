67ca6955738f7a87b85018a5d8d2390f75ec03f9
Pavel Zaytsev <pavel@iberisoft.com>
2025-08-22 16:16:36 +0200

    Merge settings

--- SapBroker/Settings.cs
+++ SapBroker/Settings.cs
@@ -5,4 +5,14 @@ class Settings
     public string SapConnectionString { get; set; }
 
     public bool SapEmulator { get; set; }
+
+    public string PacsUrl { get; set; }
+
+    public string PacsToken { get; set; }
+
+    public string FtpHost { get; set; }
+
+    public string FtpUser { get; set; }
+
+    public string FtpPassword { get; set; }
 }
