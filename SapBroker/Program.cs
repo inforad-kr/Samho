2af8d8e8154252af60f13150793e9e35b91e0df8
Pavel Zaytsev <pavel@iberisoft.com>
2025-08-22 16:18:17 +0200

    Merge top-level statements

--- SapBroker/Program.cs
+++ SapBroker/Program.cs
@@ -1,4 +1,5 @@
 using SapBroker;
+using SapBroker.Models;
 using SapBroker.Services;
 using Serilog;
 
@@ -17,6 +18,13 @@ else
 {
     builder.Services.AddScoped<ISapService, SapService>();
 }
+builder.Services.AddScoped<UploadService>();
+builder.Services.AddHttpClient("Pacs", httpClient =>
+{
+    httpClient.BaseAddress = new(settings.PacsUrl);
+    httpClient.BaseAddress = new(httpClient.BaseAddress, "api/v1/");
+    httpClient.DefaultRequestHeaders.Authorization = new("Bearer", settings.PacsToken);
+});
 
 var app = builder.Build();
 
@@ -24,10 +32,21 @@ var loggerConfig = new LoggerConfiguration();
 loggerConfig = loggerConfig.WriteTo.Console();
 Log.Logger = loggerConfig.CreateLogger();
 
+app.MapGet("/", () => "SAP Broker is running...");
+
 app.MapGet("/api/order", (ISapService sapService, int days = 1) =>
 {
     var orders = sapService.RetrieveOrders(days);
     return orders;
+});
+
+app.MapPost("/api/study", async (UploadService uploadService, StudyRef studyRef) =>
+{
+    if (studyRef.IsValid)
+    {
+        return Results.Ok(await uploadService.UploadImages(studyRef));
+    }
+    return Results.BadRequest("Invalid parameters");
 });
 
 app.Run();
